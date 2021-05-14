174b3a9f713abd7f588ceee4ff5136387a93e348
Madhu rayamajhi <yogendra@Madhus-MacBook-Air.local>
2021-05-14 20:43:37 +0545

    sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,21 +1,51 @@
-import { Avatar } from '@material-ui/core'
-import React from 'react'
+import { Avatar } from "@material-ui/core";
+import React from "react";
+import "./Sidebar.css";
 
-function Sidebar() {
-    return (
-        <div className="sidebar">  
-            <div className="sidebar__top">
-             <img src="" alt="" />
-             <Avatar/>
-             <h4>Jhabindra Rayamajhi</h4>
-             <h4>jhabindra.rayamajhi@metlife.com.np</h4>
-            </div>
-            <div className="sidebar__bootm">
+ function Sidebar() {
 
-
-            </div>
+    const recentItem =(topic)=>{
+      <p>{topic}</p>
+        // <div className="sidebar__recentItem">
+        //     <span className="sidebar__hash">#</span>
+        //     <p>{topic}</p>
+        // </div>
+    };
+   
+    
+  return (
+    <div className="sidebar">
+      <div className="sidebar__top">
+        <img
+          src="https://www.paho.org/sites/default/files/styles/flexslider_full/public/2020-06/twitter-banner-1500px.jpg?h=4f5b30f1&itok=IYdGFRGN"
+          alt=""
+        />
+        <Avatar className="sidebar__avatar" />
+        <h4>Jhabindra Rayamajhi</h4>
+        <h4>jhabindra.rayamajhi@metlife.com.np</h4>
+      </div>
+      <div className="sidebar__stats">
+        <div className="sidebar__stat">
+          <p>Who viewd you</p>
+          <p className="sidebar__statNumber">2,564</p>
         </div>
-    )
+        <div className="sidebar__stat" >
+        <p>Viewdon post</p>
+        <p className="sidebar__statNumber">2,448</p>
+        {recentItem("reactJs")}
+        </div>
+      </div>
+
+      <div className="sidebar__bottom">
+        <p>Recent</p>
+    {recentItem()}
+        {recentItem('Programming')}
+        {recentItem('softwareengineering')}
+        {recentItem('design')}
+        {recentItem('developer')}
+      </div>
+    </div>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
